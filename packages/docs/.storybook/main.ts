d937b234089404062b6a06ae3d538bf30658158d
Yann <yann_scs@hotmail.com>
2023-10-13 16:54:56 -0300

    fix: Add subpath to storybook

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -24,6 +24,12 @@ const config: StorybookConfig = {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
+  viteFinal:(config,{ configType })=>{
+    if(configType == 'PRODUCTION'){
+      config.base = '/05-design-system'
+    }
+    return config
+  },
   docs: {
     autodocs: "tag",
   },
